167a8fd1bd966605bb69e0b8182336ed710679f1
gustab-m <724905673@qq.com>
2021-09-10 14:03:11 +0800

    修改了审核，还有获取上年同期那个没改

---
+++ service/hyjj_service/src/main/java/com/hyjj/hyjjservice/controller/report/viewobject/Degital.java
@@ -0,0 +1,22 @@
+package com.hyjj.hyjjservice.controller.report.viewobject;
+
+public class Degital {
+    private Integer judge;
+    private String tailHtml;
+
+    public Integer getJudge() {
+        return judge;
+    }
+
+    public void setJudge(Integer judge) {
+        this.judge = judge;
+    }
+
+    public String getTailHtml() {
+        return tailHtml;
+    }
+
+    public void setTailHtml(String tailHtml) {
+        this.tailHtml = tailHtml;
+    }
+}
