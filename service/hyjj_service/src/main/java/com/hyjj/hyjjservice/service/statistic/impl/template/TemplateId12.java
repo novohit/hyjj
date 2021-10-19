b276d4728ee29e7d1f76ca8b34496e77ab620a23
gustab-m <724905673@qq.com>
2021-10-19 23:01:47 +0800

    首页，审核

---
+++ service/hyjj_service/src/main/java/com/hyjj/hyjjservice/service/statistic/impl/template/TemplateId12.java
@@ -0,0 +1,35 @@
+package com.hyjj.hyjjservice.service.statistic.impl.template;
+
+import com.hyjj.hyjjservice.dao.TargetKeyValueMapper;
+import com.hyjj.hyjjservice.dao.TargetValueMapper;
+import com.hyjj.hyjjservice.dataobject.StatisticsTargetKey;
+import com.hyjj.hyjjservice.service.statistic.StatisticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 模板id为11的报表的模板（海洋化工企业生产情况）
+ */
+@Component
+public class TemplateId12 extends AbstractTargetTemplate {
+    @Autowired
+    private TargetKeyValueMapper targetKeyValueMapper;
+
+    @Autowired
+    private TargetValueMapper targetValueMapper;
+
+    @Autowired
+    private StatisticService statisticService;
+
+    @Override
+    public Integer changeValue(Long reportDataId, List<Double> data) {
+        return 1;
+    }
+
+    @Override
+    public Boolean formulaVerification(List<Double> data) {
+        return data.get(0) > data.get(1);
+    }
+}
